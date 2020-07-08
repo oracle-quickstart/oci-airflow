a0b0a1f61b31bdf613a4a2f1d02a8f69a4575827
Carter Shanklin <carter@cartershanklin.com>
2020-07-08 11:36:40 -0700

    Refactored code fetch logic into a loop. Pull down some additional sample DAGs.

--- scripts/master_boot.sh
+++ scripts/master_boot.sh
@@ -433,24 +433,25 @@ mkdir -p /opt/airflow/flower
 useradd -s /sbin/nologin airflow
 chown -R airflow:airflow /run/airflow
 log "->Download OCI Hooks & Operators"
+plugin_url=https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins
 # hooks
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/hooks/oci_base.py -O /opt/airflow/plugins/hooks/oci_base.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/hooks/oci_object_storage.py -O /opt/airflow/plugins/hooks/oci_object_storage.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/hooks/oci_data_flow.py -O /opt/airflow/plugins/hooks/oci_data_flow.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/hooks/oci_data_catalog.py -O /opt/airflow/plugins/hooks/oci_data_catalog.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/hooks/oci_adb.py -O /opt/airflow/plugins/hooks/oci_adb.py
+for file in oci_base.py oci_object_storage.py oci_data_flow.py oci_data_catalog.py oci_adb.py; do
+    wget $plugin_url/hooks/$file -O /opt/airflow/plugins/hooks/$file
+done
 # operators
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/operators/oci_object_storage.py -O /opt/airflow/plugins/operators/oci_object_storage.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/operators/oci_data_flow.py -O /opt/airflow/plugins/operators/oci_data_flow.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/operators/oci_data_catalog.py -O /opt/airflow/plugins/operators/oci_data_catalog.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/operators/oci_adb.py -O /opt/airflow/plugins/operators/oci_adb.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/operators/oci_copy_object_to_adb.py -O /opt/airflow/plugins/operators/oci_copy_object_to_adb.py
+for file in oci_object_storage.py oci_data_flow.py oci_data_catalog.py oci_adb.py oci_copy_object_to_adb.py; do
+    wget $plugin_url/operators/$file -O /opt/airflow/plugins/operators/$file
+done
 # sensors
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/sensors/oci_object_storage.py -O /opt/airflow/plugins/sensors/oci_object_storage.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/plugins/sensors/oci_adb.py -O /opt/airflow/plugins/sensors/oci_adb.py
+for file in oci_object_storage.py oci_adb.py; do
+    wget $plugin_url/sensors/$file -O /opt/airflow/plugins/sensors/$file
+done
 # Airflow OCI customization
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/dags/oci_simple_example.py -O /opt/airflow/dags/oci_simple_example.py
-wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/dags/oci_advanced_example.py -O /opt/airflow/dags/oci_advanced_example.py
+dag_url=https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/dags
+for file in oci_simple_example.py oci_advanced_example.py oci_adb_sql_example.py oci_smoketest.py \
+    schedule_dataflow_app.py schedule_dataflow_with_parameters.py trigger_dataflow_when_file_exists.py; do
+    wget $dag_url/$file -O /opt/airflow/dags/$file
+done
 wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/custom/connection_form.js -O /usr/local/lib/python3.6/site-packages/airflow/www/static/connection_form.js
 wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/custom/connection.py -O /usr/local/lib/python3.6/site-packages/airflow/models/connection.py
 wget https://raw.githubusercontent.com/oracle-quickstart/oci-airflow/master/scripts/custom/www_rbac_views.py -O /usr/local/lib/python3.6/site-packages/airflow/www_rbac/views.py
