9102e949463e9c20ae49a5dddd7bf9bb38cc4d3c
Carter Shanklin <carter@cartershanklin.com>
2020-07-15 03:08:18 -0700

    Publish an XCom for the file sensor with file size.

--- scripts/plugins/sensors/oci_object_storage.py
+++ scripts/plugins/sensors/oci_object_storage.py
@@ -57,6 +57,40 @@ class BaseOCIObjectStorageSensor(BaseSensorOperator):
     def poke(self, context):
         raise Exception("Class did not implement poke method")
 
+    def list_objects(self, file, prefix_match=False):
+        hook = self.get_oci_hook()
+        namespace_name = hook.get_namespace(compartment_id=self.compartment_id)
+        object_store_client = hook.get_client(hook.oci_client)
+        base_arguments = dict(
+            bucket_name=self.bucket_name,
+            fields="size",
+            limit=100,
+            namespace_name=namespace_name,
+            prefix=file,
+        )
+        objectsummary = object_store_client.list_objects(**base_arguments)
+
+        # For exact match we only consider the first match.
+        if prefix_match == False:
+            first_match = objectsummary.data.objects[0]
+            if first_match.name == file:
+                return 1, first_match.size
+            return 0, 0
+
+        # Prefix mode: Build a list of matching files.
+        total_files = 0
+        total_size = 0
+        while True:
+            object_list = objectsummary.data
+            for object in object_list.objects:
+                total_files += 1
+                total_size += object.size
+            if object_list.next_start_with is None:
+                break
+            base_arguments["next_start_with"] = object_list.next_start_with
+            object_store_client.list_object(**base_arguments)
+        return total_files, total_size
+
     def get_oci_hook(self):
         """
         Create and return OCI Hook
@@ -78,9 +112,17 @@ class OCIObjectStorageSensor(BaseOCIObjectStorageSensor):
 
     def poke(self, context):
         self.log.info('Poking for object %s in bucket %s', self.object_name, self.bucket_name)
-        namespace_name = self.get_oci_hook().get_namespace(compartment_id=self.compartment_id)
-        return self.get_oci_hook().check_for_object(bucket_name=self.bucket_name, namespace_name=namespace_name,
-                                                    object_name=self.object_name)
+        try:
+            total_files, total_size = self.list_objects(self.prefix, prefix_match=False)
+            if total_files > 0:
+                self.log.info('Found object of size %d', total_size)
+                context['task_instance'].xcom_push('oci_storage_sensor_size', total_size)
+                return True
+            self.log.info('Object not found')
+            return False
+
+        except AirflowException as e:
+            self.log.error(e.response["Error"]["Message"])
 
 
 class OCIObjectStoragePrefixSensor(BaseOCIObjectStorageSensor):
@@ -92,40 +134,17 @@ class OCIObjectStoragePrefixSensor(BaseOCIObjectStorageSensor):
         super().__init__(*args, **kwargs)
 
     def poke(self, context):
-        self.log.info('Poking for prefix %s in bucket %s', self.prefix, self.bucket_name)
+        self.log.info('Poking for prefix [%s] in bucket %s', self.prefix, self.bucket_name)
         try:
-            hook = self.get_oci_hook()
-            namespace_name = hook.get_namespace(compartment_id=self.compartment_id)
-            object_store_client = hook.get_client(hook.oci_client)
-            base_arguments = dict(
-                namespace_name=namespace_name,
-                bucket_name=self.bucket_name,
-                prefix=self.prefix,
-                fields="size",
-            )
-            objectsummary = object_store_client.list_objects(**base_arguments)
+            total_files, total_size = self.list_objects(self.prefix, prefix_match=True)
 
-            # Build a list of matching files.
-            file_names = []
-            total_files = 0
-            total_size = 0
-            while True:
-                object_list = objectsummary.data
-                for object in object_list.objects:
-                    file_names.append(object.name)
-                    total_files += 1
-                    total_size += object.size
-                if object_list.next_start_with is None:
-                    break
-                base_arguments["next_start_with"] = object_list.next_start_with
-                object_store_client.list_object(**base_arguments)
-
-            # If we matched anything, push the list and return true.
-            if len(file_names) > 0:
-                context['task_instance'].xcom_push('oci_prefix_file_names', file_names)
+            # If we matched anything, record file count, total size and return true.
+            if total_files > 0:
+                self.log.info('Found %d objects with total size %d', total_files, total_size)
                 context['task_instance'].xcom_push('oci_prefix_total_files', total_files)
                 context['task_instance'].xcom_push('oci_prefix_total_size', total_size)
                 return True
+            self.log.info('No matching objects')
             return False
 
         except AirflowException as e:
